d8e94b5014da474aab9331d333714e10e598642a
David Weikersdorfer <davidw@danvil.de>
2012-03-04 23:14:36 +0100

    Added sub sampling Added range check

--- rgbd_features_cv/include/rgbd_features_cv/kernel2d.h
+++ rgbd_features_cv/include/rgbd_features_cv/kernel2d.h
@@ -84,18 +84,57 @@ struct Kernel2D
     }
   }
 
-  template <float (*F)(float,float,float)>
+  template <float (*F)(float,float,float), bool AxisAligned, int Samples>
   void create(const Matx22f& rotation, float ratio, float sigma) {
+//    float sum_pos = 0.0f;
+//    float sum_neg = 0.0f;
     for(int i=0; i<9; i++) {
       float y = float(i-4);
       for(int j=0; j<9; j++) {
         float x = float(j-4);
         Point2f q = rotation * Point2f(x,y);
         q.y /= ratio;
-        q = rotation.t() * q;
-        kernel[i][j] = F(sigma, q.x, q.y);
+        if(AxisAligned) {
+          q = rotation.t() * q;
+        }
+        float v;
+        if(Samples > 0) {
+          v = 0.0f;
+          for(int i=-Samples; i<=+Samples; i++) {
+            for(int j=-Samples; j<=+Samples; j++) {
+              v += F(sigma, q.x + float(j)*float(Samples)/float(2*Samples+1) , q.y + float(i)*float(Samples)/float(2*Samples+1));
+            }
+          }
+          v /= float((2*Samples + 1)*(2*Samples + 1));
+        }
+        else {
+          v = F(sigma, q.x, q.y);
+        }
+        kernel[i][j] = v / ratio;
+//        if(v >= 0.0f) {
+//          sum_pos += v;
+//        }
+//        else {
+//          sum_neg += v;
+//        }
       }
     }
+//    std::cout << "pos=" <<  sum_pos << " " << sum_neg << std::endl;
+//    for(int i=0; i<9; i++) {
+//      for(int j=0; j<9; j++) {
+//        float& v = kernel[i][j];
+//        if(v >= 0.0f) {
+//          if(sum_pos != 0) {
+//            v /= sum_pos;
+//          }
+//        }
+//        else {
+//          if(sum_pos != 0) {
+//            v /= -sum_neg;
+//          }
+//        }
+//      }
+//    }
   }
 
   template <float (*F)(float,float,float)>
@@ -105,10 +144,10 @@ struct Kernel2D
     return q;
   }
 
-  template <float (*F)(float,float,float)>
+  template <float (*F)(float,float,float), bool AxisAligned, int Samples>
   static Kernel2D Create(const Matx22f& rotation, float ratio, float sigma) {
     Kernel2D q;
-    q.create<F>(rotation, ratio, sigma);
+    q.create<F,AxisAligned,Samples>(rotation, ratio, sigma);
     return q;
   }
 
@@ -123,9 +162,9 @@ static Kernel2D sDxyKernel = Kernel2D::Create<detail::DxyKernel>(1.2f);
 
 struct Kernel2DCache
 {
-  static const float cRatioMin = 0.0f;
+  static const float cRatioMin = 0.05f;
   static const float cRatioMax = 1.0f;
-  static const int cRatioSteps = 20;
+  static const int cRatioSteps = 19;
   static const float cAngleMin = 0.0f;
   static const float cAngleMax = 3.1415f;
   static const int cAngleSteps = 30;
@@ -136,15 +175,15 @@ struct Kernel2DCache
     return convolve_impl(values, ox, oy);
   }
 
-  template <float (*F)(float,float,float)>
+  template <float (*F)(float,float,float), bool AxisAligned, int Samples>
   static Kernel2DCache Create(float sigma, const char* name) {
     Kernel2DCache q;
-    q.create<F>(sigma, name);
+    q.create<F,AxisAligned,Samples>(sigma, name);
     return q;
   }
 
 private:
-  template <float (*F)(float,float,float)>
+  template <float (*F)(float,float,float), bool AxisAligned, int Samples>
   void create(float sigma, const char* name) {
     kernel_cache_ = Mat1f(9*(cAngleSteps+1), 9*(cRatioSteps+1));
     for(int i=0; i<cAngleSteps+1; i++) {
@@ -161,7 +200,7 @@ private:
         rotation(1,1) = ca;
         rotation = rotation.t();
         std::cout << rotation(0,0) << " " << rotation(0,1) << " " << rotation(1,0) << " " << rotation(1,1) << " " << std::endl;
-        Kernel2D k2 = Kernel2D::Create<F>(rotation, ratio, sigma);
+        Kernel2D k2 = Kernel2D::Create<F,AxisAligned,Samples>(rotation, ratio, sigma);
         for(int i2=0; i2<9; i2++) {
           for(int j2=0; j2<9; j2++) {
             kernel_cache_[9*i + i2][9*j + j2] = k2.kernel[i2][j2];
@@ -174,6 +213,12 @@ private:
 
   int findRatioPos(float ratio) const {
     float p = (ratio - cRatioMin) / (cRatioMax - cRatioMin) * float(cRatioSteps);
+    if(p < 0.0f) {
+      p = 0.0f;
+    }
+    if(p > 1.0f) {
+      p = 1.0f;
+    }
     int pi = static_cast<int>(p + 0.5f); // round
     if(pi == cRatioSteps + 1) {
       pi --;
@@ -182,6 +227,9 @@ private:
   }
 
   int findAnglePos(float angle) const {
+    while(angle < 0.0f) angle += M_2_PI;
+    while(angle > M_2_PI) angle -= M_2_PI;
+    if(angle > M_PI) angle -= M_PI;
     float p = (angle - cAngleMin) / (cAngleMax - cAngleMin) * float(cAngleSteps);
     int pi = static_cast<int>(p + 0.5f); // round
     if(pi == cAngleSteps + 1) {
@@ -211,10 +259,14 @@ private:
 };
 
 
-static Kernel2DCache sLaplaceKernelCache = Kernel2DCache::Create<detail::LoG2>(1.2f, "LoG2");
-static Kernel2DCache sDxxKernelCache = Kernel2DCache::Create<detail::DxxKernel>(1.2f, "DxxKernel");
-static Kernel2DCache sDyyKernelCache = Kernel2DCache::Create<detail::DyyKernel>(1.2f, "DyyKernel");
-static Kernel2DCache sDxyKernelCache = Kernel2DCache::Create<detail::DxyKernel>(1.2f, "DxyKernel");
+static Kernel2DCache sLaplaceKernelCache = Kernel2DCache::Create<detail::LoG2, false, 1>(1.2f, "LoG2");
+static Kernel2DCache sDxxKernelCache = Kernel2DCache::Create<detail::DxxKernel, false, 1>(1.2f, "DxxKernel");
+static Kernel2DCache sDyyKernelCache = Kernel2DCache::Create<detail::DyyKernel, false, 1>(1.2f, "DyyKernel");
+static Kernel2DCache sDxyKernelCache = Kernel2DCache::Create<detail::DxyKernel, false, 1>(1.2f, "DxyKernel");
+//static Kernel2DCache sLaplaceKernelCacheNaa = Kernel2DCache::Create<detail::LoG2, false, 1>(1.2f, "LoG2 NAA");
+//static Kernel2DCache sDxxKernelCacheNaa = Kernel2DCache::Create<detail::DxxKernel, false, 1>(1.2f, "DxxKernel NAA");
+//static Kernel2DCache sDyyKernelCacheNaa = Kernel2DCache::Create<detail::DyyKernel, false, 1>(1.2f, "DyyKernel NAA");
+//static Kernel2DCache sDxyKernelCacheNaa = Kernel2DCache::Create<detail::DxyKernel, false, 1>(1.2f, "DxyKernel NAA");
 
 }
 
