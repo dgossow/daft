609fd0afb344cfecdd8d38a99ec4560a3af4e510
David Gossow <dgossow@gmail.com>
2012-03-26 21:15:26 +0200

    made daft header c++11 compatible

--- daft2/include/daft2/daft.h
+++ daft2/include/daft2/daft.h
@@ -30,31 +30,19 @@ public:
 
     enum { AUTO=-1 };
 
-    static const int DEFAULT_SCALE_LEVELS = AUTO;
-    static const float DEFAULT_BASE_SCALE = 1;
-    static const float DEFAULT_SCALE_FACTOR = 2.0;
-    static const int DEFAULT_MIN_PX_SCALE = 2;
-    static const int DEFAULT_MAX_PX_SCALE = AUTO;
-    static const int DEFAULT_DET = DET_DOB;
-    static const float DEFAULT_DET_THRESHOLD = 0.02;
-    static const int DEFAULT_PF_TYPE = PF_PRINC_CURV_RATIO;
-    static const float DEFAULT_PF_THRESHOLD = 10.0;
-    static const int DEFAULT_MAX_ALGO = MAX_FAST;
-    static const bool DEFAULT_AFFINE = true;
-
     /** default constructor */
     DetectorParams(
-        float base_scale = DEFAULT_BASE_SCALE,
-        float scale_factor = DEFAULT_SCALE_FACTOR,
-        int scale_levels = DEFAULT_SCALE_LEVELS,
-        int min_px_scale = DEFAULT_MIN_PX_SCALE,
-        int max_px_scale = DEFAULT_MAX_PX_SCALE,
-        int detector_type = DEFAULT_DET,
-        float det_threshold = DEFAULT_DET_THRESHOLD,
-        int postfilter_type = DEFAULT_PF_TYPE,
-        float pf_threshold = DEFAULT_PF_THRESHOLD,
-        int max_search_algo = DEFAULT_MAX_ALGO,
-        bool affine = DEFAULT_AFFINE):
+        float base_scale = AUTO,
+        float scale_factor = 2.0,
+        int scale_levels = AUTO,
+        int min_px_scale = 3,
+        int max_px_scale = AUTO,
+        int detector_type = DET_DOB,
+        float det_threshold = 0.02,
+        int postfilter_type = PF_PRINC_CURV_RATIO,
+        float pf_threshold = 5.0,
+        int max_search_algo = MAX_WINDOW,
+        bool affine = true):
           base_scale_(base_scale),
           scale_step_(scale_factor),
           scale_levels_(scale_levels),
