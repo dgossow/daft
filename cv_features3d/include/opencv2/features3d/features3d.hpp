9fe81948af391839ed667cbc99ad0eb8929d5cea
David Gossow <david.gossow@cs.tum.edu>
2012-05-08 14:37:58 +0200

    added cv_features3d/include/

---
+++ cv_features3d/include/opencv2/features3d/features3d.hpp
@@ -0,0 +1,13 @@
+/*
+ * features3d.h
+ *
+ *  Created on: May 2, 2012
+ *      Author: David Gossow
+ */
+
+#ifndef FEATURES3D_H_
+#define FEATURES3D_H_
+
+#include "keypoint3d.hpp"
+
+#endif /* FEATURES3D_H_ */
