7790e52468864afe4e1a525f405a495ffad7763a
David Gossow <dgossow@gmail.com>
2012-08-31 13:49:05 +0200

    added catkin project defs and rename opencv_ext to opencv_features3d

---
+++ opencv_features3d/include/opencv2/features3d/features3d.hpp
@@ -0,0 +1,14 @@
+/*
+ * features3d.h
+ *
+ *  Created on: May 2, 2012
+ *      Author: David Gossow
+ */
+
+#ifndef FEATURES3D_H_
+#define FEATURES3D_H_
+
+#include "keypoint3d.hpp"
+#include <opencv2/core/core.hpp>
+
+#endif /* FEATURES3D_H_ */
